a83911e18337e5d626f639effda844a6a1e8887c
VanshWAGH <waghvansh0@gmail.com>
2025-12-07 09:57:35 +0530

    class 2

---
+++ Class2/src/typesInTs.js
@@ -0,0 +1,6 @@
+var drink = "chai";
+var cups = Math.random() > 0.5 ? 10 : 20;
+var channelName = "chai aur code";
+var chaiFlavour = "masala chai";
+chaiFlavour = "tea";
+console.log(chaiFlavour);
---
+++ Class2/src/typesinTs.ts
@@ -0,0 +1,11 @@
+let drink = "chai";
+
+let cups = Math.random() > 0.5 ? 10 : 20;
+
+let channelName = "chai aur code";
+
+let chaiFlavour: string = "masala chai"
+
+chaiFlavour = "tea";
+
+console.log(chaiFlavour);
